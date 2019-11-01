6e794de70832b8e17112b46de27a397eb8398c9f
Peelson <nqh@meitu.com>
2019-11-01 15:38:33 +0800

    [feature] init project

---
+++ android/app/src/main/kotlin/com/peelson/awesome_flutters_4_peelson/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.peelson.awesome_flutters_4_peelson
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
